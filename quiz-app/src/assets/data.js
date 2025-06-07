f987cc09d204cae09a075e25870a8a3c5daab6ac
krishwall <krishjaiswal523@gmail.com>
2025-06-07 20:56:29 +0530

    first commit

---
+++ quiz-app/src/assets/data.js
@@ -0,0 +1,27 @@
+export const data=[
+    {
+        question: "Which device is required for internet connection?",
+        options: ["Modem", "Router", "LanCable", "Pen Drive"],
+        answer: 0
+    },
+    {
+        question: "What does HTML stand for?",
+        options: ["HyperText Markup Language", "HighText Machine Language", "HyperText and links Markup Language", "None of these"],
+        answer: 0
+    },
+    {
+        question: "Which language is used for styling web pages?",
+        options: ["HTML", "JQuery", "CSS", "XML"],
+        answer: 2
+    },
+    {
+        question: "Which is not a JavaScript framework?",
+        options: ["Python Script", "Django", "VueJS", "ReactJS"],
+        answer: 0
+    },
+    {
+        question: "What does CSS stand for?",
+        options: ["Cascading Style Sheets", "Colorful Style Sheets", "Computer Style Sheets", "Creative Style Sheets"],
+        answer: 0
+    }
+]
